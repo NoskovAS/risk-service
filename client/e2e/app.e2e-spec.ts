3d6fc9297071121eefb9c7093c50202908eecd32
Anton Noskov <noskovv.as@gmail.com>
2017-11-18 11:07:11 +0300

    Dependency update

--- client/e2e/app.e2e-spec.ts
+++ client/e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('client App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Web Service for Risk Management in the Development of Software Products');
   });
 });
